3414b72edaa2ca1d93f54a6b419d81ba99eba46a
HyHydromechanics <15075152129@163.com>
2022-07-16 17:48:29 +0800

    work down

---
+++ Code/array1.java
@@ -0,0 +1,12 @@
+public class array1 {
+    public static void main (String[]args){
+        double[] hens = {3, 4, 5, 6, 9, 0};
+        double totalWeight = 0;
+        for (int i = 0; i<=5; i++){
+            System.out.println(hens[i]);
+            totalWeight += hens[i];
+        }
+        System.out.println(totalWeight);
+        System.out.println(totalWeight/hens.length);
+    }
+}
--- note/note1.md
+++ note/note1.md
@@ -2553,3 +2553,4 @@ public class circleWork8 {
 }
 ```
 
+
