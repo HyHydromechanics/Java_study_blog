d0c955fe67842dce4747241cb507340c8b642788
HyHydromechanics <15075152129@163.com>
2022-07-23 18:20:40 +0800

    到冒泡排序的实战了

--- Code/arrayAdd2.java
+++ Code/arrayAdd2.java
@@ -9,6 +9,7 @@ public class arrayAdd2{
         for (int i = 0; i< arrOld.length; i++){
             System.out.println(arrOld[i]);
         }
+
         System.out.println("How many do you want to add: ");
         int addNum = myScanner.nextInt();
         int[] arrNew = new int[arrOld.length+addNum];
@@ -16,12 +17,15 @@ public class arrayAdd2{
         for (int i = 0; i< arrOld.length; i++){
             arrNew[i] = arrOld[i];
             System.out.println(arrNew[i]);
-        }
-        System.out.println("===");
 
-        for
-        int temp = myScanner.nextInt();
-        arrNew[3] = temp;
+        }
+        System.out.println(arrNew.length);
+
+        for (int i = arrOld.length; i< arrNew.length; i++){
+            int temp = myScanner.nextInt();
+            arrNew[i] = temp;
+        }
+
 
         for (int i = 0; i< arrNew.length; i++){
             System.out.println(arrNew[i]);
---
+++ Code/bubbleSort.java
@@ -0,0 +1,8 @@
+public class bubbleSort {
+    public static void main(String[] args){
+        int[] arr = {24,69,80,57,13};
+        for (int j = 0; j<arr.length; j++){
+
+        }
+    }
+}
