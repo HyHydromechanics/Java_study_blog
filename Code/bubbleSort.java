d4f5ec47bfdc453302962e48fc7f50c7a4fae080
HyHydromechanics <15075152129@163.com>
2022-08-01 14:24:58 +0800

    a

--- Code/bubbleSort.java
+++ Code/bubbleSort.java
@@ -1,8 +1,14 @@
 public class bubbleSort {
     public static void main(String[] args){
+        int temp = 0;
         int[] arr = {24,69,80,57,13};
         for (int j = 0; j<arr.length; j++){
-
+            if (arr[j] > arr[j+1]){
+                temp = arr [j];
+                arr [j] = arr [j+1];
+                arr [j+1] = temp;
+            }
+            System.out.println(arr[j]);
         }
     }
 }
---
+++ Code/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        int i = 1;
+        int j = 2;
+        i = j;
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
