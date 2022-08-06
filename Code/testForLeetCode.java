1614257d411a2c97503b10f9642732ad58a324e9
HyHydromechanics <87158955+HyHydromechanics@users.noreply.github.com>
2022-08-06 08:03:56 +0800

    leetcode test

---
+++ Code/testForLeetCode.java
@@ -0,0 +1,14 @@
+public class testForLeetCode {
+    public static void main (String[]args){
+        int[] num = {1,2,3};
+        int[] numNum = new int[4];
+        for (int i = 0; i<num.length; i++){
+            System.out.println("i="+ i);
+            System.out.println(num[i]+"aaa");
+            int temp;
+            temp = num[i];
+            numNum[i] = temp;
+        }
+
+    }
+}
