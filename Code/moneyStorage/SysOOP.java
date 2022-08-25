33ecea30ec270fe4713382bc49749b5fad9f435f
HyHydromechanics <87158955+HyHydromechanics@users.noreply.github.com>
2022-08-25 17:26:55 +0800

    moneyProject

--- Code/moneyStorage/SysOOP.java
+++ Code/moneyStorage/SysOOP.java
@@ -9,23 +9,28 @@ public class SysOOP {
     Scanner scanner = new Scanner(System.in);
     boolean loop = true; // switch的循环
     String command = ""; // 1～4的命令, scanner
-    String detailsTitle = "-----Details About Your Money-----";
+    String detailsTitle = "=====Details About Your Money=====";
+    String detailsTitleCopy = "=====Details About Your Money=====";
     double money = 0;
     double getMoney = 0;
-    String reasonSpendMoney = "";
+    double spendMoney = 0;
+
+    String spendMoneyNote = "Added";
+    time time = new time();
+    String timeNow = time.time;
 
 
     public void mainMenu(){
         do {
-            System.out.println("\n================零钱通菜单(OOP)===============");
-            System.out.println("\t\t\t1 零钱通明细");
-            System.out.println("\t\t\t2 收益入账");
-            System.out.println("\t\t\t3 消费");
-            System.out.println("\t\t\t4 退     出");
+            System.out.println("\n================Menu===============");
+            System.out.println("\t\t\t1 Details");
+            System.out.println("\t\t\t2 Spend");
+            System.out.println("\t\t\t3 Get");
+            System.out.println("\t\t\t4 Quit");
 
-            System.out.print("请选择(1-4): ");
+            System.out.print("Please Choose(1-4): ");
             command = scanner.next();
-            switch (command){
+            switch (command) {
                 case "1":
                     this.details();
                     break;
@@ -37,35 +42,70 @@ public class SysOOP {
                     break;
                 case "4":
                     this.Quit();
+                    break;
             }
         } while (loop);
     }
 
     public void details(){
-        System.out.println("details");
+        System.out.println("-----details-----");
+        if (detailsTitle.equals(detailsTitleCopy)){
+            System.out.println("You do not have any activities yet");
+        } else System.out.println(detailsTitle);
     }
 
     public void SpendMoney(){
-        System.out.println("Spend Money");
+        System.out.println("-----Spend Money-----");
+        System.out.print("How much do you need to spend:");
+        spendMoney = scanner.nextDouble();
+        if (spendMoney<=0 || spendMoney>=money){
+            spendMoney = 0;
+            System.out.println("You can's spend more money than you have");
+        } else {
+            System.out.print("For What: ");
+            spendMoneyNote = scanner.next();
+            money -= spendMoney;
+            detailsTitle += "\nmoney you spent\t-" + spendMoney + "\t"+timeNow + "\t\t" + money + "\t" +spendMoneyNote;
+            System.out.println(detailsTitle);
+        }
     }
 
     public void GetMoney(){
-        System.out.println("Get Money");
+        System.out.println("-----Get Money-----");
+        System.out.print("How much money have you received: ");
+        getMoney = scanner.nextDouble();
+        if (getMoney<=0){
+            System.out.println("You cannot have less than zero");
+            getMoney =0;
+        } else if (getMoney>=10000){
+            System.out.println("We cannot receive so much money");
+            getMoney = 0;
+        }
+        money += getMoney;
+        detailsTitle += "\nmoney you get\t+" + getMoney + "\t"+timeNow + "\t\t" + money+ "\t" + spendMoneyNote;
+        System.out.println(detailsTitle);
     }
 
     public void Quit(){
-        System.out.println("Quit");
+        System.out.println("-----Quit-----");
+        System.out.println("Are you sure you want to quit this program?\n(Y/n)");
+        String yes = "y";
+        String Yes = "Y";
+        String no = "n";
+        String quitCommand = scanner.next();
+        if (quitCommand.equals(yes) || quitCommand.equals(Yes)){
+            loop = false;
+            System.out.println("Goodbye");
+        } else if (quitCommand.equals(no)){
+            loop = true;
+        } else System.out.println("Please type Y/n to conform");
+
     }
 
 
-    public class time{
+    public static class time{
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String time = dateFormat.format(date.getTime());
     }
 }
-
-
-
-
-
---
+++ Code/moneyStorage/SysOOP_APP.java
@@ -0,0 +1,7 @@
+package moneyStorage;
+
+public class SysOOP_APP {
+    public static void main(String[] args) {
+        new SysOOP().mainMenu();
+    }
+}
