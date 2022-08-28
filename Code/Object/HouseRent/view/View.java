ece3ffcf4fd959f1b798c2c974266c04861cd40c
HyHydromechanics <87158955+HyHydromechanics@users.noreply.github.com>
2022-08-28 16:46:11 +0800

    房屋出租，zip包是自己写的fw“

--- Code/Object/HouseRent/view/View.java
+++ Code/Object/HouseRent/view/View.java
@@ -1,5 +1,11 @@
 package Object.HouseRent.view;
 
 public class View {
-    
+    private boolean loop = true;
+    private char key;
+    public void mainMenu(){
+        do{
+            System.out.println("房屋出租系统菜单");
+        } while (loop);
+    }
 }
