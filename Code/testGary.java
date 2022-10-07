c183793a56ac51a8457d14da5a7658d96a5e8ffa
HyHydromechanics <87158955+HyHydromechanics@users.noreply.github.com>
2022-10-06 20:39:34 -0700

    a

--- Code/testGary.java
+++ Code/testGary.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class testGary {
+import java.util.Scanner;
+public class testGary {
+    public static void main(String[] args){
+        int[] num = {1,2,3,4,5,6,7,8,9,10};
+        System.out.print("a"+num.length);
+        Scanner scanner = new Scanner(System.in);
+        int numFind = scanner.nextInt();
+
+        for (int i = 0; i<num.length; i++){
+            if (numFind == num[i]){
+                if (i>6){
+                    int temp = i+3;
+                    System.out.println(i);
+                    int outPut = (num.length-temp)*(-1);
+                    System.out.println(num[outPut]);
+                } else {
+                    System.out.println(i);
+                    System.out.println(num[i+3]);
+                }
+            }
+        }
+    }
 }
