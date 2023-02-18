c1ac48108b7ae5467b738ff685b71497d670ab75
HyHydromechanics <87158955+HyHydromechanics@users.noreply.github.com>
2023-02-18 10:47:49 -0800

    a

--- Code/if01.java
+++ Code/if01.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 public class if01 {
     public static void main(String[] args) {
-        // 编写一个程序, 判断人的年龄是否大于18岁
-        // 思路分析: 
-        // 1. 接受输入的年龄, 定义scanner对象
-        // 2. 保存年龄到变量
-        // 3. if判断, 并且输出
         Scanner myScanner = new Scanner(System.in);
-        System.out.print("YourAge?");
-        int age = myScanner.nextInt();
-        if (age>18){
-            System.out.print("Your age is larger than 18");
+        System.out.print("Selector?");
+        int sel = myScanner.nextInt();
+        if (sel == 1){
+            System.out.print(" B value ");
+        } else if (sel == 0) {
+            System.out.print(" A value");
+        } else {
+            System.out.println("incorrect output");
         }
-        System.out.print("Continue...");
     }
 }
